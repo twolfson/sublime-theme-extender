5811ad60e106d40b19574a0b155755b69510c826
Todd Wolfson <todd@twolfson.com>
2014-10-24 04:31:13 -0700

    Planned out remainder of save

--- theme-extender.py
+++ theme-extender.py
@@ -4,12 +4,13 @@ import sublime
 import sublime_plugin
 
 SUBLIME_ROOT = path.join(sublime.packages_path(), '../')
-THEME_EXTENDER_FOLDER = 'Packages/User/Theme Extender/'
-THEME_EXTENDER_FULL_FOLDER = path.join(SUBLIME_ROOT, 'Packages/User/Theme Extender/')
+THEME_EXTENDER_FOLDER = path.join('Packages', 'User', 'Theme Extender')
+THEME_EXTENDER_FULL_FOLDER = path.join(SUBLIME_ROOT, THEME_EXTENDER_FOLDER)
 
 class ThemeExtender():
     @classmethod
     def guarantee_theme_extender_folder(cls):
+        """Guarantee the theme extender folder exists so we can write files to it"""
         if not path.exists(THEME_EXTENDER_FULL_FOLDER):
             makedirs(THEME_EXTENDER_FULL_FOLDER)
 
@@ -56,8 +57,32 @@ class ThemeExtenderExtendThemeCommand(sublime_plugin.WindowCommand):
         extension_filepath = path.join(THEME_EXTENDER_FOLDER,
             scheme_filename_root + '.extended' + scheme_filename_ext + '.plist')
         extension_full_filepath = path.join(SUBLIME_ROOT, extension_filepath)
+        # TODO: On fresh buffer add in explanation comments and reference to original file so we an extend it
         self.window.open_file(extension_full_filepath)
 
 class ThemeExtenderListener(sublime_plugin.EventListener):
     def on_post_save(self, view):
-        print 'haaaai'
+        """When a save occurs to a ThemeExtender extension, re-extend the theme"""
+        # If we are in the wrong folder, exit early
+        filepath = view.file_name()
+        if not filepath.startswith(THEME_EXTENDER_FULL_FOLDER):
+            return
+
+        # If the file is not an extension, exit early
+        filename = path.basename(filepath)
+        if not filename.endswith('.plist'):
+            return
+
+        # TODO: Attempt to load the file as a plist
+
+        # TODO: If we cannot, complain to the user
+
+        # TODO: Determine the source theme
+
+        # TODO: Attempt to load source theme
+
+        # TODO: If we cannot, complain to the user
+
+        # TODO: Extend the extensions onto the theme
+
+        # TODO: Save the updated theme
