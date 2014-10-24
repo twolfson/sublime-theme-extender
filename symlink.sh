d638dce9135fc10e30303f77ad9432979a08b783
Todd Wolfson <todd@twolfson.com>
2014-10-24 04:47:06 -0700

    Adding symlink for theme

--- symlink.sh
+++ symlink.sh
@@ -1,1 +1,4 @@
-ln -s $PWD ~/.config/sublime-text-2/Packages/theme-extender
+ln -s "$PWD" "~/.config/sublime-text-2/Packages/theme-extender"
+mkdir -p "Packages/User/Theme Extender"
+theme_name="Monokai Extended Bright.extended.tmTheme.plist"
+ln -s "$PWD/$theme_name" "~/.config/sublime-text-2/Packages/theme-extender/$theme_name"
