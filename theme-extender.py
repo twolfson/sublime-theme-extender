f87b42a384651ad39d8475e8396972aa7f8d6406
Todd Wolfson <todd@twolfson.com>
2014-10-24 03:08:44 -0700

    Adding in commands and getting this party started

--- theme-extender.py
+++ theme-extender.py
@@ -1,7 +1,7 @@
 import sublime_plugin
 
 
-class ThemeExtenderCommand(sublime_plugin.WindowCommand):
+class ThemeExtenderExtendThemeCommand(sublime_plugin.WindowCommand):
     def run(self):
         """Be awesome to each other."""
-        pass
+        print "hai"
